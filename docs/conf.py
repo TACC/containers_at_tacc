9d031d828d95372ab1cec08f2be362c20e26f65b
Erik Ferlanti <eferlanti@tacc.utexas.edu>
2021-09-23 19:05:08 -0500

    added tacc-ml image table to GPU section (#5)
    added a "building a GPU container" section

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+# -- RTD configuration -------------------------------------------------------
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 # -- Project information -----------------------------------------------------
 
@@ -76,7 +78,14 @@ html_logo = 'images/TACC-White-No-Mask.png'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
+html_theme = 'default'
+
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+else:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -173,6 +182,7 @@ epub_title = project
 
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
+
 
 # To allow css styling
 def setup(app):
